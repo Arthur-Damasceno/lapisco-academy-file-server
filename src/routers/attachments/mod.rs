3fd3edc650da13a74f7c6a7be858a6fe2e3a303b
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-07-20 13:45:48 -0300

    fix: upload attachment max file size

--- src/routers/attachments/mod.rs
+++ src/routers/attachments/mod.rs
@@ -12,9 +12,9 @@ const BODY_LIMIT: usize = 1024 * 1_000_000;
 
 pub fn router() -> Router {
     Router::new()
-        .layer(DefaultBodyLimit::max(BODY_LIMIT))
         .route("/:id", get(download::handle))
         .route("/:id/info", get(information::handle))
         .route("/", post(upload::handle))
+        .layer(DefaultBodyLimit::max(BODY_LIMIT))
         .route("/:id", delete(remove::handle))
 }
